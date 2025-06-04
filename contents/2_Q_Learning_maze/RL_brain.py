a442533cd2d5373acf2f9b934aa92e2bbc738b22
jianhui.zhao <jianhui.zhao@mintegral.com>
2025-06-04 19:21:07 +0800

    refactor: optimize state addition in QLearningTable.

--- contents/2_Q_Learning_maze/RL_brain.py
+++ contents/2_Q_Learning_maze/RL_brain.py
@@ -42,10 +42,9 @@ class QLearningTable:
     def check_state_exist(self, state):
         if state not in self.q_table.index:
             # append new state to q table
-            self.q_table = self.q_table.append(
-                pd.Series(
-                    [0]*len(self.actions),
-                    index=self.q_table.columns,
-                    name=state,
-                )
+            new_state = pd.Series(
+                [0]*len(self.actions),
+                index=self.q_table.columns,
+                name=state,
             )
+            self.q_table = pd.concat([self.q_table, new_state.to_frame().T])
